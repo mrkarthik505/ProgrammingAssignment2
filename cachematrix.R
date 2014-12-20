7d6345610bc74001f1c3370aeff5f17f8a0475d4
mrkarthik505 <mrkarthik505@gmail.com>
2014-12-20 16:48:59 +0530

    Completed assmt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here we are trying to cache the inverse of the square invertible matrix
+## for that we are creating two functions one is the cache matrix and other
+## function is to solve the inverse of the matrix
 
-## Write a short comment describing this function
+## This function caches the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ mr <- NULL
+ set <- function(y) {
+   x <<- y
+   mr <<- NULL
+ }
+ get <- function() x
+ setrev <- function(rev) mr <- rev
+ getrev <- function() mr
+ list(set = set,get = get, setrev = setrev , getrev = getrev)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix returned by the makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mr <- x$getrev()
+  if(!is.null(mr)){
+    message("getting cached data")
+    return(mr)
+  }
+  mat <- x$get()
+  mr <- solve(mat, ...)
+  x$setrev(mr)
+  mr
 }
